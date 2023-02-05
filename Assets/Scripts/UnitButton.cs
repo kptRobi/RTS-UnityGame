3c5fcfb1fd5e5e7bcd11b59249384bf849a0c257
kptRobi <kptRobi@gmail.com>
2023-02-05 17:06:04 +0100

    piniondze

--- Assets/Scripts/UnitButton.cs
+++ Assets/Scripts/UnitButton.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitButton : MonoBehaviour {
 
-	public void SpawnUnit(GameObject prefab)
+    [SerializeField]
+    GameObject spawnPrefab;
+
+    Button button;
+    Text text;
+
+    private void Awake()
     {
-        CameraControl.SpawnUnits(prefab);
+        button = GetComponentInChildren<Button>(true);
+        text = GetComponentInChildren<Text>(true);
+        Buyable buyable;
+        if (spawnPrefab && (buyable = spawnPrefab.GetComponent<Buyable>()))
+        {
+            button.image.sprite = buyable.icon;
+        }
+    }
+
+    private void Update()
+    {
+        Buyable buyable;
+        if (spawnPrefab && (buyable = spawnPrefab.GetComponent<Buyable>()))
+        {
+            text.text = buyable.cost + "W€";
+            button.interactable = Money.HaveEnoughMoney(buyable.cost);
+        }
+    }
+
+
+    public void SpawnUnit()
+    {
+        CameraControl.SpawnUnits(spawnPrefab);
     }
 }
